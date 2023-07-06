27c129259966cd68b2126a41de7eba89615b68bc
sgkrish19 <137775828+sgkrish19@users.noreply.github.com>
2023-07-06 17:12:59 +0530

    Update App.java

--- App.java
+++ App.java
@@ -3,3 +3,22 @@ public class App {
         System.out.println("Hello, World!");
     }
 }
+package Abstraction;
+
+abstract class app{  
+        abstract void draw();  
+        }  
+        class Rectangle extends Shape{  
+        void draw(){System.out.println("drawing rectangle");}  
+        }  
+        class Circle1 extends Shape{  
+        void draw(){System.out.println("drawing circle");}  
+        }  
+        public class Abstract {
+        public static void main(String args[]){  
+        Shape s=new Circle1(); 
+        s.draw();  
+        Shape s1=new Rectangle(); 
+        s1.draw(); 
+        }  
+        } 
