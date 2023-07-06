009b82523c5bd0487636c697780d70f1584fa786
sgkrish19 <137775828+sgkrish19@users.noreply.github.com>
2023-07-06 17:14:09 +0530

    Update App.java

--- App.java
+++ App.java
@@ -1,6 +1,6 @@
 package Abstraction;
 
-abstract class app{  
+abstract class Shape{  
         abstract void draw();  
         }  
         class Rectangle extends Shape{  
@@ -9,7 +9,7 @@ abstract class app{
         class Circle1 extends Shape{  
         void draw(){System.out.println("drawing circle");}  
         }  
-        public class Abstract {
+        public class App {
         public static void main(String args[]){  
         Shape s=new Circle1(); 
         s.draw();  
