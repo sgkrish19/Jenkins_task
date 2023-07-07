4ac400f02010411413e18d7288197d01b2fdc280
sgkrish19 <137775828+sgkrish19@users.noreply.github.com>
2023-07-07 11:51:32 +0530

    Update App.java

--- App.java
+++ App.java
@@ -1,8 +1,5 @@
         public class App {
         public static void main(String args[]){  
-        Shape s=new Circle1(); 
-        s.draw();  
-        Shape s1=new Rectangle(); 
-        s1.draw(); 
+        System.out.println("Hello World...........");
         }  
         } 
